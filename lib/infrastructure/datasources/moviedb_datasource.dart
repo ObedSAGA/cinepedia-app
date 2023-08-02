10ba2d591c94cd81d976c7b76aee384ae3657ef6
ObedSAGA <obedsg95@gmail.com>
2023-08-02 12:58:58 +0200

    Add repositories implementation and print list movies

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -1,6 +1,8 @@
-import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:dio/dio.dart';
 
+import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_response.dart';
 import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 
@@ -14,7 +16,13 @@ class MoviedbDataSource extends MoviesDataSource {
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
     final response = await dio.get('/movie/now_playing');
-    final List<Movie> movies = [];
+    final movieDbResponse = MovieDbResponse.fromJson(response.data);
+
+    final List<Movie> movies =
+        movieDbResponse.results
+        .where((moviedb) => moviedb.posterPath != 'no-poster')
+        .map(
+          (moviedb) => MovieMapper.movieDBtoEntity(moviedb)).toList();
     return movies;
   }
 }
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreenMovies extends StatelessWidget {
   static const name = 'homeScreen-movies';
@@ -9,7 +11,40 @@ class HomeScreenMovies extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
         body: Scaffold(
-      body:  Placeholder(),
+      body: _HomeView(),
     ));
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView();
+
+  @override
+  _HomeVIewState createState() => _HomeVIewState();
+}
+
+class _HomeVIewState extends ConsumerState<_HomeView> {
+  @override
+  void initState() {
+    super.initState();
+
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+
+    if (nowPlayingMovies.isEmpty) return const CircularProgressIndicator();
+
+    return ListView.builder(
+      itemCount: nowPlayingMovies.length,
+      itemBuilder: (context, index) {
+        final movie = nowPlayingMovies[index];
+        return ListTile(
+          title: Text(movie.title),
+        );
+      },
+    );
   }
 }
