d85fa3456fe6290c000b2201181c44a5c9cd6f14
ObedSAGA <obedsg95@gmail.com>
2023-08-11 13:17:36 +0200

    Avoid simultaneous requests at infinitescroll

--- lib/presentation/providers/movies/movies_providers.dart
+++ lib/presentation/providers/movies/movies_providers.dart
@@ -5,18 +5,18 @@ import '../../../domain/entities/movie.dart';
 
 final nowPlayingMoviesProvider =
     StateNotifierProvider<MoviesNotifier, List<Movie>>((ref) {
-    
-    final fetchMoreMovies = ref.watch(movieRepositoryProvider).getNowPlaying;
-      
-    return MoviesNotifier(
-      fetchMoreMovies: fetchMoreMovies,
-    );
+  final fetchMoreMovies = ref.watch(movieRepositoryProvider).getNowPlaying;
+
+  return MoviesNotifier(
+    fetchMoreMovies: fetchMoreMovies,
+  );
 });
 
 typedef MovieCallBack = Future<List<Movie>> Function({int page});
 
 class MoviesNotifier extends StateNotifier<List<Movie>> {
   int currentPage = 0;
+  bool isLoading = false;
   MovieCallBack fetchMoreMovies;
 
   MoviesNotifier({
@@ -24,8 +24,13 @@ class MoviesNotifier extends StateNotifier<List<Movie>> {
   }) : super([]);
 
   Future<void> loadNextPage() async {
+    if (isLoading) return;
+
+    isLoading = true;
     currentPage++;
     final List<Movie> movies = await fetchMoreMovies(page: currentPage);
     state = [...state, ...movies];
+    await Future.delayed(const Duration(milliseconds: 300));
+    isLoading = false;
   }
 }
