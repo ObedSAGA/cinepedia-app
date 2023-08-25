95a25a9d6cf0dcda1ccf215fc8342b9786f8644f
ObedSAGA <obedsg95@gmail.com>
2023-08-25 13:05:02 +0200

    Modify router config to add movie screen

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,12 +1,21 @@
 import 'package:cinemapedia/presentation/screens/screens.dart';
 import 'package:go_router/go_router.dart';
 
-final appRouter = GoRouter(
-  routes: [
-    GoRoute(
-      path: '/',
-      name: HomeScreenMovies.name,
-      builder: (context, state) => const HomeScreenMovies(),
-    ),
-  ]
-  );
+final appRouter = GoRouter(routes: [
+  GoRoute(
+    path: '/',
+    name: HomeScreenMovies.name,
+    builder: (context, state) => const HomeScreenMovies(),
+  ),
+  GoRoute(
+    path: '/movie/:id',
+    name: MovieScreen.name,
+    builder: (context, state) {
+      final movieId = state.pathParameters['id'] ?? 'no-id';
+
+      return  MovieScreen(
+        movieId: movieId,
+      );
+    },
+  ),
+]);
