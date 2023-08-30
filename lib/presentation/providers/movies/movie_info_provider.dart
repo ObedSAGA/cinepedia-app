d7a6abe8aeb741577c1e561d3aa37f25cec2f97e
ObedSAGA <obedsg95@gmail.com>
2023-08-30 12:43:39 +0200

    Implements HTTP request in movie screen

--- lib/presentation/providers/movies/movie_info_provider.dart
+++ lib/presentation/providers/movies/movie_info_provider.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../domain/entities/movie.dart';
 
-final movieInfoProvider = StateNotifierProvider((ref) {
+final movieInfoProvider = StateNotifierProvider<MovieMapNotifer, Map<String, Movie>>((ref) {
   final movieRepository = ref.watch(movieRepositoryProvider);
 
   return MovieMapNotifer(getMovie: movieRepository.getMovieById);
--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,19 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:cinemapedia/presentation/providers/movies/movie_info_provider.dart';
 
-class MovieScreen extends StatelessWidget {
+import '../../../domain/entities/movie.dart';
+
+class MovieScreen extends ConsumerStatefulWidget {
   static const name = "movie-screen";
 
-  final String movieId; 
+  final String movieId;
 
-  const MovieScreen({
-    super.key, required this.movieId
-  });
+  const MovieScreen({super.key, required this.movieId});
+
+  @override
+  MovieScreenState createState() => MovieScreenState();
+}
+
+class MovieScreenState extends ConsumerState<MovieScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    ref.read(movieInfoProvider.notifier).loadMovie(widget.movieId);
+  }
 
   @override
   Widget build(BuildContext context) {
+    final Movie? movie = ref.watch(movieInfoProvider)[widget.movieId];
+
+    if (movie == null) {
+      return const Scaffold(body: Center(child: CircularProgressIndicator(strokeWidth: 2)));
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('MovieID: $movieId'),
+        title: Text('MovieID: ${widget.movieId}'),
       ),
     );
   }
